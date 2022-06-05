25e6932f7526860d4ebfcb2dc07357df8bed1000
Tu <tht157@hotmail.de>
2022-06-05 21:17:03 +0200

    improve SVG structure

--- components/TapeSprite.vue
+++ components/TapeSprite.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tape />
+  <Tape ref="tapeSprite" />
 </template>
 
 <script setup lang="ts">
 import Tape from '@/assets/tape.svg?component';
+
+const tapeSprite = ref<SVGElement>(null);
 </script>
 
 <style scoped>
