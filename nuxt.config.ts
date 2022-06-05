7c1b63007f6db53dec54f6c306114464d44a729b
Tu <tht157@hotmail.de>
2022-06-05 22:56:47 +0200

    see last commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ import svg from "vite-svg-loader";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['nuxt-windicss', '@vueuse/nuxt'],
+  mode: 'spa',
+  buildModules: ['nuxt-windicss', '@vueuse/nuxt', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
       'spotify-client-id': '1fbac5c6a38344dc8ed9234a65c152d7',
