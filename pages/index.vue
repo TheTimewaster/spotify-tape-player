159673b8a8d44b1e5eee555175a9a7e1c901967c
Tu <tht157@hotmail.de>
2022-06-22 01:15:09 +0200

    refactor spotify plugin

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
       <template v-if="currentPlaybackStore.hasPlayback">
         <tape-sprite class="w-full mb-8" />
         <currently-playing class="rounded-md w-full" />
-        <button class="mt-2 rounded px-4 py-2 bg-dark-500 text-white" @click="player.togglePlay">
+        <button class="mt-2 rounded px-4 py-2 bg-dark-500 text-white" @click="togglePlay">
           {{ currentPlaybackStore.isPlaying ? 'Pause' : 'Play' }}
         </button>
       </template>
@@ -32,67 +32,16 @@ definePageMeta({
   },
 });
 
+const { $getSpotifyPlayer } = useNuxtApp();
 const currentPlaybackStore = useCurrentPlaybackStore();
-const { $spotify } = useNuxtApp();
-const accessToken = useCookie('spotify-access-token');
-const player = ref<Spotify.Player>(null);
 
-useHead({
-  title: 'Home',
-  script: [
-    {
-      src: 'https://sdk.scdn.co/spotify-player.js',
-    },
-  ],
-});
+const togglePlay = () => {
+  $getSpotifyPlayer().togglePlay();
+};
 
 const logout = () => {
-  player.value.disconnect();
+  $getSpotifyPlayer().disconnect();
   useCookie('spotify-access-token', undefined);
   navigateTo('/login');
 };
-
-const initSpotifyPlayer = () => {
-  window.onSpotifyWebPlaybackSDKReady = () => {
-    player.value = new Spotify.Player({
-      name: '[TEST] Tape Player',
-      getOAuthToken: (cb) => cb(accessToken.value),
-      volume: 0.5,
-    });
-
-    player.value.on('player_state_changed', async (event) => {
-      currentPlaybackStore.playback = event;
-
-      if (
-        // sometimes event and other properties can be null for some reason
-        event == null ||
-        event.context == null ||
-        event.context.uri == null ||
-        // make sure that the fetch for context is only called once
-        (currentPlaybackStore.context != null && currentPlaybackStore.context.uri === event.context.uri)
-      )
-        return;
-
-      if (event.context.uri.startsWith('spotify:album')) {
-        const albumId = event.context.uri.split(':')[2];
-        const album = await $spotify.getAlbum(albumId);
-        currentPlaybackStore.context = album;
-      } else if (event.context.uri.startsWith('spotify:playlist')) {
-        const playlistId = event.context.uri.split(':')[2];
-        const playlist = await $spotify.getPlaylist(playlistId);
-        currentPlaybackStore.context = playlist;
-      } else if (event.context.uri.startsWith('spotify:artist')) {
-        const artistId = event.context.uri.split(':')[2];
-        const artist = await $spotify.getArtist(artistId);
-        currentPlaybackStore.context = artist;
-      }
-    });
-
-    player.value.connect();
-  };
-};
-
-onMounted(() => {
-  initSpotifyPlayer();
-});
 </script>
---
+++ plugins/spotify.client.ts
@@ -0,0 +1,70 @@
+import { useCurrentPlaybackStore } from '~~/store/playing-now';
+import SpotifyWeb from 'spotify-web-api-js';
+
+export default defineNuxtPlugin(() => {
+  const accessToken = useCookie('spotify-access-token');
+  const currentPlaybackStore = useCurrentPlaybackStore();
+
+  const spotifyWeb = new SpotifyWeb();
+
+  if (accessToken.value != null) {
+    spotifyWeb.setAccessToken(accessToken.value);
+  }
+
+  useHead({
+    title: 'Home',
+    script: [
+      {
+        src: 'https://sdk.scdn.co/spotify-player.js',
+        body: true,
+      },
+    ],
+  });
+
+  let spotifyPlayer;
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    spotifyPlayer = new Spotify.Player({
+      name: '[TEST] Tape Player',
+      getOAuthToken: (cb) => cb(accessToken.value),
+      volume: 0.5,
+    });
+
+    spotifyPlayer.on('player_state_changed', async (event) => {
+      currentPlaybackStore.playback = event;
+
+      if (
+        // sometimes event and other properties can be null for some reason
+        event == null ||
+        event.context == null ||
+        event.context.uri == null ||
+        // make sure that the fetch for context is only called once
+        (currentPlaybackStore.context != null && currentPlaybackStore.context.uri === event.context.uri)
+      )
+        return;
+
+      if (event.context.uri.startsWith('spotify:album')) {
+        const albumId = event.context.uri.split(':')[2];
+        const album = await spotifyWeb.getAlbum(albumId);
+        currentPlaybackStore.context = album;
+      } else if (event.context.uri.startsWith('spotify:playlist')) {
+        const playlistId = event.context.uri.split(':')[2];
+        const playlist = await spotifyWeb.getPlaylist(playlistId);
+        currentPlaybackStore.context = playlist;
+      } else if (event.context.uri.startsWith('spotify:artist')) {
+        const artistId = event.context.uri.split(':')[2];
+        const artist = await spotifyWeb.getArtist(artistId);
+        currentPlaybackStore.context = artist;
+      }
+    });
+
+    spotifyPlayer.connect();
+  };
+
+  return {
+    provide: {
+      spotifyWeb,
+      // TODO: this is a very hacky way to get the player instance, maybe refactor to a getter instead?!
+      getSpotifyPlayer: () => spotifyPlayer
+    },
+  };
+});
